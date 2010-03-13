aca7e7b2b177e2325c02978bb9dc24dabdbb8be5
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2010-03-13 14:01:44 +0000

    Will probabaly use python for Makefile

--- Makefile.py
+++ Makefile.py
@@ -1,5 +1,21 @@
 #!/bin/env python
+import os
+import sys
+import shutils
 
 PREFIX = '/usr/local/'
 DATADIR = PREFIX + 'share/Battleships/'
 GRAPHICSDIR = DATADIR + 'Graphics/'
+BINDIR = PREFIX + 'bin/'
+
+def install():
+	os.mkdirs(GRAPHICSDIR)
+	shutils
+
+if __name__ == "__main__":
+	if os.getuid() != 0:
+		sys.exit('Must be run as root')
+	try:
+		eval(sys.argv[1])()
+	except NameError:
+		sys.exit('%s is not a valid function' % sys.argv[1])
